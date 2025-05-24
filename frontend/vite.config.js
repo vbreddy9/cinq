079d35ef7fdfcbe5374c5c445c2259e686b688f3
vbreddy9 <111076954+vbreddy9@users.noreply.github.com>
2025-05-24 10:25:41 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist' // Vercel expects the build output here
-  },
-  base: './' // ensures relative paths for assets in production
 });
